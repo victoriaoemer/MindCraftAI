e1ca6605989673e8febd3b5fe3ed43fe43141550
Victoria Oemer <victoria.oemer@gmail.com>
2024-11-05 10:15:46 +0100

    add interactivity to nodes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,53 @@
-import { ReactFlow, Controls, Background } from "@xyflow/react";
+"use client";
+import { useState, useCallback } from "react";
+import {
+  ReactFlow,
+  Controls,
+  Background,
+  applyNodeChanges,
+  applyEdgeChanges,
+} from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
 
-const nodes = [
+const initialNodes = [
   {
     id: "1",
-    position: { x: 0, y: 0 },
     data: { label: "Hello" },
+    position: { x: 0, y: 0 },
     type: "input",
   },
   {
     id: "2",
-    position: { x: 100, y: 100 },
     data: { label: "World" },
+    position: { x: 100, y: 100 },
   },
 ];
 
-const edges = [{ id: "1-2", source: "1", target: "2" }];
+const initialEdges = [{ id: "1-2", source: "1", target: "2" }];
 
 function Flow() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes: any) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    []
+  );
+  const onEdgesChange = useCallback(
+    (changes: any) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    []
+  );
+
   return (
     <div className="h-screen">
-      <ReactFlow nodes={nodes} edges={edges} colorMode="dark">
+      <ReactFlow
+        colorMode="dark"
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        fitView
+      >
         <Background />
         <Controls />
       </ReactFlow>
